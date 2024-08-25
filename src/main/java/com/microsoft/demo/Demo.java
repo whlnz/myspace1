78a753749484019df810dc8944ff5b43356710e3
Roman Korobitsin <strongerdevteams@gmail.com>
2024-08-25 10:34:38 +1000

    commit

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -1,11 +1,20 @@
 package com.microsoft.demo;
 
+import org.springframework.core.enums.LabeledEnum;
+
 public class Demo {
+
     public void DoSomething(boolean flag){
+
+
+        System.out.println(LabeledEnum.LABEL_ORDER);
+        //log.debug();
         if(flag){
             System.out.println("I am covered");
             return;
         }
+
+
 
         System.out.println("I am not covered");
     }
